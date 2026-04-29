efa5c87540c1c4cff39227242c7b086f1b5f220d
Abhishek Shivangi <131172976+AbhishekShivangi@users.noreply.github.com>
2026-04-29 13:08:43 +0530

    Create sms_model.py

---
+++ sms_model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+data = [
+    ("Win ₹5000 now click here http://fake.com", 1),
+    ("Your OTP is 123456", 0),
+    ("Verify your bank account immediately", 1),
+    ("Meeting at 5pm", 0)
+]
+
+df = pd.DataFrame(data, columns=["text", "label"])
+
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(df["text"])
+y = df["label"]
+
+model = MultinomialNB()
+model.fit(X, y)
+
+pickle.dump(model, open("sms_model.pkl", "wb"))
+pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
+
+print("✅ SMS model created")
