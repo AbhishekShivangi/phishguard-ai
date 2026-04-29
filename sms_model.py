89bdad05d47182abe07d9d4e0163a17871e4d1f2
Abhishek Shivangi <131172976+AbhishekShivangi@users.noreply.github.com>
2026-04-29 16:49:34 +0530

    Update sms_model.py

--- sms_model.py
+++ sms_model.py
@@ -1,8 +1,11 @@
+# SMS PHISHING MODEL (TF-IDF + Naive Bayes)
+
 import pandas as pd
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
+# Sample dataset (replace with Kaggle dataset later)
 data = [
     ("Win ₹5000 now click here http://fake.com", 1),
     ("Your OTP is 123456", 0),
@@ -12,7 +15,7 @@ data = [
 
 df = pd.DataFrame(data, columns=["text", "label"])
 
-vectorizer = TfidfVectorizer()
+vectorizer = TfidfVectorizer(stop_words='english')
 X = vectorizer.fit_transform(df["text"])
 y = df["label"]
 
@@ -22,4 +25,4 @@ model.fit(X, y)
 pickle.dump(model, open("sms_model.pkl", "wb"))
 pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
 
-print("✅ SMS model created")
+print("✅ SMS model trained")
